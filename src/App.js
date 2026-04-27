f6832b228e414848c19de4918475afb1c51dd1d5
KenaDholakiya <kenadholakiyavbi@gmail.com>
2026-04-27 18:05:20 +0530

    textutils

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+    <Navbar title="TextUtils" aboutText="About"/>
+    {/*<Navbar title="TextUtils" />*/}
+
+     {/*<Navbar/>*/}
+    </>
+  );
+}
+
+export default App;
