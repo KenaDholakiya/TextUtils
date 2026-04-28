998320e8e9075096d758827f3c4d1813b220a9ad
KenaDholakiya <kenadholakiyavbi@gmail.com>
2026-04-28 16:55:49 +0530

    Alerts-Routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,70 @@
 import "./App.css";
-// import About from "./components/About";
+import Alert from "./components/Alert";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');//checking dark mode 
-  const toggleMode =()=>{
-    if(mode === 'light')
-    {
-      setMode('dark');
-      document.body.style.backgroundColor='#042743';
+  //useState for mode change
+  const [mode, setMode] = useState("light"); //checking dark mode
+  //useState for Alert
+  const [alert, setAlert] = useState(null);
+
+  //alert setmethod
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+  //mode change method
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+      // setInterval(() => {
+      //   document.title='TextUtils is Amazing';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title='Install TextUtils Now';
+      // }, 1500);
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
     }
-    else
-    {
-      setMode('light')
-      document.body.style.backgroundColor='white';
-    }
-  }
+  };
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
-      {/*<Navbar/>*/}
-      <div className="container my-3" >
-        <TextForm heading="Enter the text to analyze below" mode={mode}/>
-        {/*<About/>*/}
-      </div>
+      <Router>
+        <Navbar
+          title="TextUtils"
+          aboutText="About"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        {/*<Navbar/>*/}
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />}/>
+            <Route exact path="/" element={<TextForm
+                showAlert={showAlert}
+                heading="Enter the text to analyze below"
+                mode={mode}
+              />}/>
+            
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,37 +1,51 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
+  // convert to upercase function
   const handleUpClick = () => {
     //console.log("Uppercase was Clicked" + text);
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to uppercase!","success");
   };
+  // convert to Lowercase function
   const handleLoClick = () => {
     //console.log("Uppercase was Clicked" + text);
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to lowercase!","success");
   };
+  // copy to text function
   const handleCopyClick = () => {
     console.log("I am Copy");
     var text=document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to Clipboard!","success");
+
   };
+  //clear text function
   const handleClearClick = () => {
     //console.log("Uppercase was Clicked" + text);
     let newText = "";
     setText(newText);
+    props.showAlert("Text Cleared!","success");
   };
+  //remove extra spaces function
   const handleExtraSpeces = () => {
     //console.log("Uppercase was Clicked" + text);
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "));
+    props.showAlert("Removed Extraspaces!","success");
   };
+  //to capitalize first letter of every sentences function
   const handleSenCaseClick = () => {
     //console.log("Uppercase was Clicked" + text);
     let newText = text.charAt(0).toUpperCase()+text.slice(1);
     setText(newText);
+    props.showAlert("Converted to Sentance case!","success");
   };
+  //on change handle function
   const handleOnChange = (event) => {
     //console.log("OnChange");
     setText(event.target.value);
@@ -41,6 +55,7 @@ export default function TextForm(props) {
   //setText("new Text") ;//Correct  way to change text
   return (
     <>
+    {/* Text Container */}
       <div className="container" style={{color:props.mode==='dark'?'white':'#042743'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
@@ -53,6 +68,8 @@ export default function TextForm(props) {
             style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'#042743'}}
           ></textarea>
         </div>
+
+        {/* Button of change different Text */}
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>
           Convert to Uppercase
         </button>
@@ -72,14 +89,21 @@ export default function TextForm(props) {
           Remove Extra Spaces
         </button>
       </div>
+      {/* Summary container */}
       <div className="container my-3" style={{color:props.mode==='dark'?'white':'#042743'}}>
         <h2>Your text summary</h2>
+        {/* Counting word and lettter */}
         <p>
-          {text.split(" ").length} words and {text.length} characters
+          {text.split(/\s+/).filter((element)=>{
+            return element.length !==0}).length} words and {text.length} characters
         </p>
+
+        {/* Reading time */}
         <p>
           {0.008 * text.split(" ").length} Minutes Read
         </p>
+
+        {/* Preview of Entered Text */}
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
       </div>
